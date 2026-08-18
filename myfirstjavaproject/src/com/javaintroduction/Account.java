2afb85ac1e4190f5ed18995a7a187f5eded13b66
kvenkatanagarjunareddy <nagarjjuna481@gmail.com>
2026-08-18 18:07:39 +0530

    commit

--- myfirstjavaproject/src/com/javaintroduction/Account.java
+++ myfirstjavaproject/src/com/javaintroduction/Account.java
@@ -2,45 +2,45 @@ package com.javaintroduction;
 
 public class Account {
 	String Name;
-	int Balance ;
+	int Balance;
 	boolean Account_Status;
-	static int Account_Number=100;
+	
+	static int Account_Number = 100;
+	
+	
 	{
 		Account_Number++;
 	}
+	
 	void displaydetails() {
 		System.out.println(Name);
 		System.out.println(Balance);
 		System.out.println(Account_Status);
 		System.out.println(Account_Number);
-		System.out.println("----------------");
-
+		System.out.println("--------------------");
 	}
 
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+	
 		Account obj1 = new Account();
-		obj1.Name = "Nagarjuna";
-		obj1.Balance = 34000;
+		obj1.Name = "Virat";
+		obj1.Balance = 45000;
 		obj1.Account_Status = true;
-		obj1.displaydetails();
+		obj1.displaydetails(); 
+		
 		
 		Account obj2 = new Account();
-		obj2.Name = "Nagarjuna";
-		obj2.Balance = 34000;
+		obj2.Name = "Rohit";
+		obj2.Balance = 52000;
 		obj2.Account_Status = false;
-		obj2.displaydetails();
-
-		System.out.println(obj1.Account_Status);
-		Account obj3 = new Account();
-		obj3.Name = "Nagarjuna";
-		obj3.Balance = 34000;
-		obj3.Account_Status = true;
+		System.out.println(obj1.Account_Status); 
+		obj2.displaydetails(); 
 		
-		obj3.displaydetails();
-
+		Account obj3 = new Account();
+		obj3.Name = "Rahul";
+		obj3.Balance = 28000;
+		obj3.Account_Status = true;
+		obj3.displaydetails(); 
 	}
-
 }
