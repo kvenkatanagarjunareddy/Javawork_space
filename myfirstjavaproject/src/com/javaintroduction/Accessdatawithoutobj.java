2286d436d7f61eae69f167dad98ac5b50964aa66
kvenkatanagarjunareddy <nagarjjuna481@gmail.com>
2026-08-19 17:10:47 +0530

    commit

---
+++ myfirstjavaproject/src/com/javaintroduction/Accessdatawithoutobj.java
@@ -0,0 +1,40 @@
+package com.javaintroduction;
+
+public class Accessdatawithoutobj {
+	
+	static {
+		new Accessdatawithoutobj();
+		}
+	{
+		method1();
+		method2();
+		method3();
+		method4();
+	}
+
+	void method1() {
+		System.out.println("Method 1 is executed");
+
+	}
+
+	void method2() {
+		System.out.println("Method 2 is executed");
+
+	}
+
+	void method3() {
+		System.out.println("Method 3 is executed");
+
+	}
+
+	void method4() {
+		System.out.println("Method 4 is executed");
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
