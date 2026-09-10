b94e52a9189352cfb5dec82b8802eb9f034afeee
kvenkatanagarjunareddy <nagarjjuna481@gmail.com>
2026-09-10 11:34:23 +0530

    commit

--- myfirstjavaproject/src/com/javaintroduction/GradeCalculator.java
+++ myfirstjavaproject/src/com/javaintroduction/GradeCalculator.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 
 public class GradeCalculator {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter your marks!: ");
-		int marks=sc.nextInt();
-		if(marks<0 || marks>100 ){
-			System.out.println(" plase enter the valid marks");
-		}
-		else if(marks>90 && marks<=100) {
-			 System.out.println("Congratulations!.. you got grade (A)");
-		 }else if(marks>75 && marks<=90) {
-			 System.out.println("Good you got grade (B)");
-		 }else if(marks>60 && marks<75) {
-			 System.out.println("you got grade (C)");
-		 }else if(marks>40 && marks<60) {
-			 System.out.println("you got grade (D)");
-		 }else {
-			 System.out.println("you are fail! study more");
-		 }
-		sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt();
 
-	}
+        
+        if (marks < 0 || marks > 100) {
+            System.out.println("Please enter valid marks between 0 and 100.");
+        } else if (marks > 90) {
+            System.out.println("Congratulations!.. you got grade (A)");
+        } else if (marks > 75) {
+            System.out.println("Good, you got grade (B)");
+        } else if (marks > 60) {
+            System.out.println("You got grade (C)");
+        } else if (marks >= 40) { 
+            System.out.println("You got grade (D)");
+        } else {
+            System.out.println("You failed! Study harder next time.");
+        }
 
+        sc.close(); 
+    }
 }
