b4597d37cd7f61724bbe1c235d78086e2d0db73e
kvenkatanagarjunareddy <nagarjjuna481@gmail.com>
2026-09-17 11:54:09 +0530

    commit

---
+++ myfirstjavaproject/src/com/logicalStatements/forloops/FactorsOfNumber.java
@@ -0,0 +1,22 @@
+package com.logicalStatements.forloops;
+
+import java.util.Scanner;
+
+public class FactorsOfNumber {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		factors(num);
+	}
+	static void factors(int num) {
+		for(int i=1;i<=num/2;i++) {
+			if(num%i==0) {
+				System.out.print(i+" ");
+			}
+		}
+		System.out.print(num);
+	}
+}
+	
+
