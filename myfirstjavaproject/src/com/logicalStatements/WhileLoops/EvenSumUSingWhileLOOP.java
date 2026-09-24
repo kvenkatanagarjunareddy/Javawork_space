3073a830a0027bff598d52505d0d88e47f482d97
kvenkatanagarjunareddy <nagarjjuna481@gmail.com>
2026-09-24 20:00:59 +0530

    commit

---
+++ myfirstjavaproject/src/com/logicalStatements/WhileLoops/EvenSumUSingWhileLOOP.java
@@ -0,0 +1,29 @@
+
+package com.logicalStatements.WhileLoops;
+
+import java.util.Scanner;
+
+public class EvenSumUSingWhileLOOP {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a num: ");
+		int num = sc.nextInt();
+		SeperateDigitPrint(num);
+
+	}
+
+	static void SeperateDigitPrint(int num) {
+		String numStr = Integer.toString(num);
+		int i = 0;
+		int sum = 0;
+		while (i < numStr.length()) {
+			int digit = numStr.charAt(i) - '0';
+			sum += digit;
+			i += 2;
+		}
+		System.out.println("The sum of digit is " + sum);
+
+	}
+
+}
